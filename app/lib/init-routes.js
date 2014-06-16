3bc34375d72160d3a75c64dc311eb8a78ef93639
Courey Elliott <coureyelliott@gmail.com>
2014-06-16 16:54:41 -0500

    init

---
+++ app/lib/init-routes.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var traceur = require('traceur');
+var dbg = traceur.require(__dirname + '/route-debugger.js');
+var initialized = false;
+
+module.exports = (req, res, next)=>{
+  if(!initialized){
+    initialized = true;
+    load(req.app, next);
+  }else{
+    next();
+  }
+};
+
+function load(app, fn){
+  var home = traceur.require(__dirname + '/../routes/home.js');
+  var users = traceur.require(__dirname + '/../routes/users.js');
+  var passport = require('passport');
+  require('../config/passport')(passport);
+
+  app.get('/', dbg, home.index);
+
+  app.get('/login', dbg, users.login);
+  app.get('/signup', dbg, users.signup);
+  app.get('/profile', dbg, isLoggedIn, users.profile);
+  app.get('/logout', dbg, users.logout);
+
+  app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
+
+  app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
+
+	// FACEBOOK ROUTES
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	// TWITTER ROUTES
+	app.get('/auth/twitter', passport.authenticate('twitter'));
+
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+  console.log('Routes Loaded');
+  fn();
+}
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
